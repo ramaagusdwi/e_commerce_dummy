0821dfe6361d158d8094b83ff80e0199a1936548
ramap <shinratensei>
2022-01-03 10:48:49 +0700

    fix looping to show item gridview

--- lib/ui/page/home/favorite_view.dart
+++ lib/ui/page/home/favorite_view.dart
@@ -40,27 +40,12 @@ class FavoriteView extends StatelessWidget {
                     mainAxisSpacing: 10,
                     crossAxisCount: 2,
                     children: [
-                      for (int i = 0;
-                          i < state.favoriteProductList.length - 1;
-                          i++)
+                      for (int i = 0; i < state.favoriteProductList.length; i++)
                         ItemShoesCard(
                           state.favoriteProductList[i],
                         )
                     ]),
               )
-            // Container(
-            //             // height: 276,
-            //             height: 230,
-            //             child: ListView.builder(
-            //                 shrinkWrap: true,
-            //                 scrollDirection: Axis.horizontal,
-            //                 itemCount: state.favoriteProductList.length,
-            //                 itemBuilder: (BuildContext context, int index) =>
-            //                     ItemShoesCard(
-            //                       state.favoriteProductList[index],
-            //                     )),
-            //           )
-
             : state.state == ResultStateFavorite.Failed
                 ? Center(
                     child: vText(state.message,
