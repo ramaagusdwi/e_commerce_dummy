774982172531bc21cee5c0902d275f487d51e529
rama agus <ramap664@gmail.com>
2022-07-19 11:31:23 +0700

    delete implement package overlay_tooltip add package tutorial coach mark

--- lib/ui/page/home/home_page.dart
+++ lib/ui/page/home/home_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:overlay_tooltip/overlay_tooltip.dart';
 import 'package:provider/provider.dart';
 import 'package:test_mobile_apps_dev/provider/home_model.dart';
 import 'package:test_mobile_apps_dev/resources/colors.dart';
 import 'package:test_mobile_apps_dev/ui/page/home/account_view.dart';
 import 'package:test_mobile_apps_dev/ui/page/home/product_view.dart';
-import 'package:test_mobile_apps_dev/ui/widget/coachmark_ballon.dart';
-import 'package:test_mobile_apps_dev/ui/widget/custom_tooltip.dart';
 
 import 'favorite_view.dart';
 
@@ -21,23 +18,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late HomeModel homeModel;
-
-  final TooltipController _controller = TooltipController();
   bool done = false;
 
   @override
-  void initState() {
-    // _controller.start(); // starts tooltip display from the beginning
-    _controller.onDone(() {
-      setState(() {
-        done = true;
-      });
-    });
-  }
+  void initState() {}
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
@@ -59,20 +46,7 @@ class _HomePageState extends State<HomePage> {
         child: Consumer<HomeModel>(
           builder: (context, home, child) {
             homeModel = home;
-            return OverlayTooltipScaffold(
-              startWhen: (initializedWidgetLength) async {
-                await Future.delayed(const Duration(milliseconds: 500));
-                return initializedWidgetLength == 1 && !done;
-              },
-              overlayColor: ColorSource.blackOverlayBg,
-              tooltipAnimationCurve: Curves.linear,
-              tooltipAnimationDuration: const Duration(milliseconds: 1000),
-              controller: _controller,
-              builder: (BuildContext context) {
-                return buildScaffold(_widgetOptions, _onItemTapped);
-              },
-            );
-            // return buildScaffold(_widgetOptions, _onItemTapped);
+            return buildScaffold(_widgetOptions, _onItemTapped);
           },
         ));
   }
@@ -82,34 +56,12 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         backgroundColor: ColorSource.white,
         body: SafeArea(
-          child: Stack(
-            children: [
-              Center(
-                child: _widgetOptions.elementAt(homeModel.index),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(32.0),
-                child: CoachMarkBallon(),
-              ),
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: OverlayTooltipItem(
-                      tooltipVerticalPosition: TooltipVerticalPosition.TOP,
-                      displayIndex: 0,
-                      tooltip: (controller) => Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15, bottom: 50),
-                            child: MTooltip(
-                                title:
-                                    'Lihat daftar produk yang ingin kamu beli disini',
-                                controller: controller),
-                          ),
-                      child: Container())),
-            ],
+          child: Center(
+            child: _widgetOptions.elementAt(homeModel.index),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
+          items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.view_list),
               label: 'Product',
